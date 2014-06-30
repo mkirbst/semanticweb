10432dc07e49637ede1dcb76698fe7d2d8d283ce
mkirbst <mkirbstqgmail.com>
2014-06-30 18:22:48 +0200

    fixed faulty rdf exporter

--- code/dnbrdf/Main.java
+++ code/dnbrdf/Main.java
@@ -26,13 +26,13 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		//Map<String, emp>empsgnb = htwk_json2hmap("http://141.57.21.45:8080/info/staff", "<gndo:preferredNameForThePerson>", "</gndo:preferredNameForThePerson>");
+		Map<String, emp>empsgnb = htwk_json2hmap("http://141.57.21.45:8080/info/staff", "<gndo:preferredNameForThePerson>", "</gndo:preferredNameForThePerson>");
 		
-		//hmap2rdf(empsgnb, "htwkstaff.rdf");
-		//dnb_gnd2rdf(empsgnb, "/home/m/Downloads/GND.rdf", "HTWK_GND.rdf");
+		hmap2rdf(empsgnb, "htwkstaff.rdf");
+		dnb_gnd2rdf(empsgnb, "/home/m/Downloads/GND.rdf", "HTWK_GND.rdf");
 		
 		Map<String, emp>empsdnb = htwk_json2hmap("http://141.57.21.45:8080/info/staff", "<rdf:li>", "</rdf:li>");
-		dnb_dnb2rdf(empsdnb, "/home/m/Downloads/DNBTitel.rdf", "HTWK_DNB.rdf");
+		dnb_dnb2rdf(empsdnb, "/home/m/Downloads/DNBTitel.rdf", "HTWK_DNBTitel.rdf");
 		
 		
 	}
@@ -69,8 +69,6 @@ public static boolean dnb_dnb2rdf(Map<String, emp> hDnbEmps, String dnbfile, Str
 		
 		while ((line = br.readLine()) != null) {
 			String linetrimmed = line.trim();	// performance
-			if(linetrimmed.contains(nametag)) 
-				System.out.println(linetrimmed);
 			
 			// jump in triple
 			if(linetrimmed.startsWith(begintag)) 
@@ -95,7 +93,7 @@ public static boolean dnb_dnb2rdf(Map<String, emp> hDnbEmps, String dnbfile, Str
 				//t3.append(line);
 				//ts3+=line;
 				if(found == true)  {
-					System.out.println(hits + " " + t3.toString());
+					System.out.println("DNB match: "+ hits + " " + t3.toString());
 					writer.write(t3.toString());
 				} else {
 					//System.out.println(n);
@@ -118,7 +116,7 @@ public static boolean dnb_dnb2rdf(Map<String, emp> hDnbEmps, String dnbfile, Str
 	
 	}
 	
-	public static boolean dnb_gnd2rdf(Map<String, emp> hEmps, String dnbfile, String outfile) throws IOException {
+	public static boolean dnb_gnd2rdf(Map<String, emp> hEmps, String gndfile, String outfile) throws IOException {
 	
 	//preprare filewriter
 	BufferedWriter writer;
@@ -140,7 +138,7 @@ public static boolean dnb_dnb2rdf(Map<String, emp> hDnbEmps, String dnbfile, Str
 	String ts3 = "";
 	boolean found = false;
 	
-	BufferedReader br = new BufferedReader(new FileReader("/home/m/Downloads/GND.rdf"));
+	BufferedReader br = new BufferedReader(new FileReader(gndfile));
 	String line;
 	
 	while ((line = br.readLine()) != null) {
@@ -170,7 +168,7 @@ public static boolean dnb_dnb2rdf(Map<String, emp> hDnbEmps, String dnbfile, Str
 			//t3.append(line);
 			//ts3+=line;
 			if(found == true)  {
-				System.out.println(hits + " " + t3.toString());
+				System.out.println("GND match: " + hits + " " + t3.toString());
 				writer.write(t3.toString());
 			} else {
 				//System.out.println(n);
@@ -232,7 +230,6 @@ public static boolean dnb_dnb2rdf(Map<String, emp> hDnbEmps, String dnbfile, Str
 	        			 				0);
 	        	 //DNB-GND.rdf name format as hasmap key value
 	        	 hEmps.put(starttag+name+endtag, tmpemp);
-	        	 System.out.println(starttag+name+endtag);
 	        	 n++;
 	         }
 	      } catch (ParseException pe) {
@@ -263,9 +260,6 @@ public static boolean dnb_dnb2rdf(Map<String, emp> hDnbEmps, String dnbfile, Str
 		while(itr.hasNext()) 
 		{
 			emp tEmp = (emp) ((Entry) itr.next()).getValue();
-		
-			//System.out.println("rdfwriter processing cuid" + tEmp.cuid);
-			
 			writer.write("  <semweb:Employee rdf:about=\"https://raw.githubusercontent.com/mkirbst/semanticweb/master/htwkstaff.json#cuid" + tEmp.cuid + "\">\n");
 			writer.write("    <semweb:cuid>"+tEmp.cuid+"</semweb:cuid>\n");
 			writer.write("    <semweb:lastname>"+tEmp.lastname+"</semweb:lastname>\n");
